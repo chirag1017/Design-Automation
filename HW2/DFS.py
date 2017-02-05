971f10e9af7955b1278ee8c0ebfba5eaefc1a146
DESKTOP-QG2DL2H\chira <shahc@oregonstate.edu>
2017-02-05 15:39:30 -0800

    DFS first commit

---
+++ HW2/DFS.py
@@ -0,0 +1,27 @@
+'''DFS implementation of automated design of gear trains'''
+
+gears= [11,23,31,47,59,71,83,97,109,127]
+wout= [-117,77,377,-20,-2345,2]
+
+'''class Node:
+    def __init__(self,gears,wout):
+        self.gears= gears
+        self.wout=wout '''
+cond = 'true'
+
+'''while cond=='true':'''
+
+
+class Node:
+    def __init__(self,gears,wout):
+        self.gears=gears
+        self.wout=wout
+
+class
+
+
+class DFS:
+    def __init__(self):
+
+    def create_children(self,gears):
+        if()
